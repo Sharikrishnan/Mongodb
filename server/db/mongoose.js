50630ecf0d804b5b26cf38f2e8673f4acb06aa18
Harikrishnan Senthil <sharikrishnan@athenahealth.com>
2019-01-06 23:20:45 +0530

    Add Get /todos/id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise= global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodosApp');
+mongoose.connect('mongodb://localhost:27017/TodosApp',{ useNewUrlParser: true });
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var bodyParser =require('body-parser');
+var {ObjectID} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose')
+var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -24,6 +25,21 @@ app.post('/todos',(req,res)=>{
 app.get('/todos',(req,res) => {
     Todo.find().then((todos)=>{
         res.send({todos});
+    },(e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id',(req,res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send('Not valid one');
+    }
+    Todo.findById(id).then((todo)=>{
+        if (!todo) {
+            res.status(404).send('File not Found');
+        }
+        res.send({todo});
     },(e) => {
         res.status(400).send(e);
     });
