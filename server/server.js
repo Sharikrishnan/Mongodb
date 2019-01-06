5bde31dd5503ef1ee05ac737a493eae69d97b3a8
Harikrishnan Senthil <sharikrishnan@athenahealth.com>
2019-01-06 20:08:08 +0530

    ad Get todos

--- server/server.js
+++ server/server.js
@@ -17,8 +17,16 @@ app.post('/todos',(req,res)=>{
     todo.save().then((docs)=>{
         res.send(docs);
     },(e)=>{
-        res.status(400).send(e)
+        res.status(400).send(e);
     })
+});
+
+app.get('/todos',(req,res) => {
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    },(e) => {
+        res.status(400).send(e);
+    });
 });
 
 app.listen(3000,() => {
