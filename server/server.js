87c9913f118ed1a9320159489176746044557555
Harikrishnan Senthil <sharikrishnan@athenahealth.com>
2019-01-07 01:10:38 +0530

    Add patch API

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var bodyParser =require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser =require('body-parser');
+const {ObjectID} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 var app = express();
 
@@ -34,6 +35,7 @@ app.get('/todos',(req,res) => {
 
 app.get('/todos/:id',(req,res) => {
     var id = req.params.id;
+    console.log(id);
     if (!ObjectID.isValid(id)) {
         return res.status(404).send('Not valid one');
     }
@@ -62,6 +64,32 @@ app.delete('/todos/:id',(req,res) => {
     });
 });
 
+app.patch('/todos/:id',(req,res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body,['text','completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Not valid one');
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id,{
+        $set : body
+    }, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+})
 
 app.listen(port,() => {
     console.log(`Started on port ${port} `);
