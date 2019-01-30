8cdf97acd4bcbccf0b99b55f1d9ecab2594b2b88
Harikrishnan Senthil <sharikrishnan@athenahealth.com>
2019-01-30 19:03:20 +0530

    swagger changes

--- server/app.js
+++ server/app.js
@@ -2,15 +2,20 @@ require('./config/config');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const swagger = require('swagger-ui-express');
+const yaml = require('yamljs');
 
 const { apiRoutes } = require('./api/routes');
 
+const swaggerDocs = yaml.load('../swagger.yaml');
 
 const app = express();
 
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocs));
 
 const routes = apiRoutes();
 
